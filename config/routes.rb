cad17c9d8c9e0272e2055ee3d71a0a9b028a4012
diemntn-05102000 <ngocdiem5102000@gmail.com>
2022-08-09 10:45:40 +0700

    Chapter 11

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
     resources :users
+    resources :account_activations, only: %i(edit)
     post "/signup", to: "users#create"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
