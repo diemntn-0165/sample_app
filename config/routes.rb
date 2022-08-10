5599bf113d1208739882493ef691062ec83f79ac
diemntn-05102000 <ngocdiem5102000@gmail.com>
2022-08-10 15:37:07 +0700

    Chapter_13

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :users
     resources :account_activations, only: %i(edit)
     resources :password_resets, only: %i(new create edit update)
+    resources :microposts, only: %i(create destroy)
     post "/signup", to: "users#create"
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
